871a608d5b480f89fae2f118d68271dad3b404c5
Danke <danik2007.kov@gmail.com>
2024-11-09 14:20:34 +0300

    test

---
+++ scripts/function1.js
@@ -0,0 +1,6 @@
+export default function() {
+    let string = prompt("Введите строку:")
+    const el = document.querySelector(".block-text");
+    el.innerText = string;
+    elem.style.color = "black";
+} 
